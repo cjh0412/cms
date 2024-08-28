b79ac2f7aadf48fba121690c3e6d027a35976758
jyedev04 <jyedev04@gmail.com>
2024-08-29 03:57:47 +0900

    user-api 수정  - service 테스트 코드 수정

--- user-api/src/main/java/com/cjhdev/cms/user/domain/customer/ChangeBalanceForm.java
+++ user-api/src/main/java/com/cjhdev/cms/user/domain/customer/ChangeBalanceForm.java
@@ -1,8 +1,14 @@
 package com.cjhdev.cms.user.domain.customer;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChangeBalanceForm {
     private String from;
     private String message;
